7f8bf5b213a90e28e9af1576d75a0f3112d5f8c6
Saïkou2469 <114421724+Projectsul22@users.noreply.github.com>
2025-05-01 20:06:56 +0200

    update offre

--- jo_app/urls.py
+++ jo_app/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('inscription/', views.inscription, name='inscription'),
     path('connexion/', views.connexion_utilisateur, name='connexion'),
     path('accueil/', views.accueil, name='accueil'),
+    path('offres/', views.offres, name='offres'),
 
     path('deconnexion/', views.deconnexion_utilisateur, name='deconnexion'),
 ]
